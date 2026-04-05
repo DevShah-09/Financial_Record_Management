eafe892b82b487f99e3580bd17c4f69984111437
Dev Shah <devmshah09@gmail.com>
2026-04-05 19:46:53 +0530

    automatically add Render hostname to ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-insecure-key')
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Security Settings for Production
 if not DEBUG:
